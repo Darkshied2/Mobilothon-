a44254e862177cea651d8b9312017f7a18c8f871
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-11-08 21:00:48 +0530

    feat: Add tailwindCss

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,9 +3,9 @@ import '../styles/Home.css'; // Custom CSS for additional styling
 
 function Home() {
     return (
-       <>
-       <h1>hello world</h1>
-       </>
+        <>
+            <h1>hello world</h1>
+        </>
     );
 }
 
